3b72c98b4399942f53020ee7f3d3c800cbb106ab
grambo <gramham@live.com>
2021-04-13 15:00:57 -0600

    updated paint and smoke to work with new system

--- paintAndSmoke.py
+++ paintAndSmoke.py
@@ -2,7 +2,7 @@ import bpy
 
 
 class objectInfo:
-  def __init__(self, initialObject, length, startFrame, endFrame, name, curve, height, smokeobject, paintobject):
+  def __init__(self, initialObject, length, startFrame, endFrame, name, curve, height, smokeObject, paintObject):
     self.initialObject = initialObject
     self.length = length
     self.startFrame = startFrame
@@ -10,7 +10,8 @@ class objectInfo:
     self.name = name
     self.height = height
     self.curve = curve
-    
+    self.smokeobject = smokeObject
+    self.paintObject = paintObject
     
 objectArrayInfo = []  
 
@@ -28,38 +29,57 @@ def recreateObjectArray ():
        newObject = objectInfo(objectName, lengthArray[index], startFrameArray[index], endFrameArray[index], nameArray[index], curveArray[index], heightArray[index], smokeObjectArray[index], paintObjectArray[index])
        objectArrayInfo.append(newObject)
        
-recreateObjectArray()   
 
-#foo_objs2 = [obj.name for obj in bpy.data.objects if obj.name.startswith("Sphere")]
-#print(foo_objs2)
-
-def addpaintbrushes():
-  for name in foo_objs2:
-    some_obj = bpy.data.objects[name]
+def addPaintBrushes(objectName, start, end):
+    some_obj = bpy.data.objects[objectName]
+    startframe = start - 1  
     bpy.context.view_layer.objects.active = some_obj
-    print(some_obj)
-  
     some_obj.modifiers.new(name='Dynamic Paint', type='DYNAMIC_PAINT')
-    some_obj.modifiers.new(name='Smoke', type='SMOKE')
-    some_obj.modifiers["Smoke"].smoke_type = 'FLOW'
     some_obj.modifiers['Dynamic Paint'].ui_type = 'BRUSH' 
     bpy.ops.dpaint.type_toggle(type='BRUSH') 
-    bpy.context.object.modifiers["Smoke"].flow_settings.smoke_color = (0.7, 0.195761, 0.115268) 
     some_obj.modifiers['Dynamic Paint'].brush_settings.paint_color = (0, 0, 0)                                                    
     some_obj.modifiers['Dynamic Paint'].brush_settings.paint_source = 'VOLUME_DISTANCE'            
     some_obj.modifiers["Dynamic Paint"].brush_settings.invert_proximity = True
     some_obj.modifiers["Dynamic Paint"].brush_settings.paint_alpha = 0
-    some_obj.modifiers["Dynamic Paint"].brush_settings.keyframe_insert(data_path="paint_alpha", frame=186)
+    some_obj.modifiers["Dynamic Paint"].brush_settings.keyframe_insert(data_path="paint_alpha", frame= startframe)
     some_obj.modifiers["Dynamic Paint"].brush_settings.paint_alpha = 1
-    some_obj.modifiers["Dynamic Paint"].brush_settings.keyframe_insert(data_path="paint_alpha", frame=196)
+    some_obj.modifiers["Dynamic Paint"].brush_settings.keyframe_insert(data_path="paint_alpha", frame= start)
+    some_obj.modifiers["Dynamic Paint"].brush_settings.paint_alpha = 0
+    some_obj.modifiers["Dynamic Paint"].brush_settings.keyframe_insert(data_path="paint_alpha", frame= end)
     some_obj.modifiers["Dynamic Paint"].brush_settings.paint_distance = 0.001
     bpy.context.view_layer.objects.active = None
-        
-def deletepaint():
-    for name in foo_objs2:
-       some_obj = bpy.data.objects[name]
-       bpy.context.view_layer.objects.active = some_obj
-       if "Dynamic Paint" in some_obj.modifiers:
-              some_obj.modifiers.remove(some_obj.modifiers['Dynamic Paint'])
 
-addpaintbrushes()
+def addSmoke(objectName, start, end):
+    some_obj = bpy.data.objects[objectName]
+    startframe = start - 1  
+    bpy.context.view_layer.objects.active = some_obj
+    some_obj.modifiers.new(name='Smoke', type='FLUID')
+    some_obj.modifiers['Smoke'].fluid_type = 'FLOW'
+    some_obj.modifiers['Smoke'].flow_settings.flow_type = 'SMOKE'
+    
+    #some_obj.modifiers["Dynamic Paint"].brush_settings.paint_alpha = 0
+    #some_obj.modifiers["Dynamic Paint"].brush_settings.keyframe_insert(data_path="paint_alpha", frame= startframe)
+    #some_obj.modifiers["Dynamic Paint"].brush_settings.paint_alpha = 1
+    #some_obj.modifiers["Dynamic Paint"].brush_settings.keyframe_insert(data_path="paint_alpha", frame= start)
+    #some_obj.modifiers["Dynamic Paint"].brush_settings.paint_alpha = 0
+    #some_obj.modifiers["Dynamic Paint"].brush_settings.keyframe_insert(data_path="paint_alpha", frame= end) 
+    
+recreateObjectArray()  
+
+class TestObjectInfo:
+  def __init__(self, startFrame, endFrame, smokeObject, paintObject):
+    self.startFrame = startFrame
+    self.endFrame = endFrame
+    self.smokeObject = smokeObject
+    self.paintObject = paintObject
+    
+#newobject = TestObjectInfo(0, 100, "SmokeSphere", "Sphere")   
+#objectArrayInfo.append(newobject)
+
+for objectInfo in objectArrayInfo:
+    start = objectInfo.startFrame
+    end = objectInfo.endFrame
+    smokeObject = objectInfo.smokeObject
+    paintObject = objectInfo.paintObject
+    addPaintBrushes(paintObject, start, end)
+    #addSmoke(smokeObject, start, end)
